21089d40d20b7bc0361295234848c4d47afd3369
Jared Cross <cross.jared@gmail.com>
2015-10-08 23:34:25 -0600

    socket integrations

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.sendFile(path.join(__dirname, '../parasplat', 'index.html'));
+server.listen(8080);
+
+
+router.get('/', function(req, res,next) {
+    res.sendFile('index.html');
 });
 
+io.on('connection', function(client) {
+    console.log('Client connected...');
 
+    client.on('join', function(data) {
+        console.log(data);
+        client.emit('messages', 'Hello from server');
+    });
+
+});
 
 module.exports = router;
