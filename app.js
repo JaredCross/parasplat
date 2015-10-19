2bd2aebab7a136d8638a5c6c38297b6653b9ca17
Jared Cross <cross.jared@gmail.com>
2015-10-19 15:15:47 -0600

    testing

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ app.get('/auth/google',
   app.get('/auth/google/callback',
   passport.authenticate('google', { failureRedirect: '/login' }),
   function(req, res) {
-    res.redirect('/');
+    res.redirect('#/');
   });
 
 // app.use(function (req, res, next) {
