f079ad67aa255c74e02eab7620c2f07a31aa2051
Jared Cross <cross.jared@gmail.com>
2015-10-20 06:31:33 -0600

    test

--- js/main.js
+++ js/main.js
@@ -233,45 +233,48 @@ Main.prototype = {
 
       //gameover
       if(p1Alive.ready && p2Alive.ready) {
-        if (!p1Alive.alive && !p2Alive.alive) {
-          gameOverMsg = me.game.add.text(600, 600, 'Everybody Lose!', style);
-          newGameButton=game.add.button(450, 200, 'button', newGame);
-          gameoverMsg.fixToCamera = true;
-          newGameButton.fixToCamera = true;
-        } else if (!p1Alive.alive && p2Alive.alive) {
-          if (playerNumber === 2) {
-            gameOverMsg = me.game.add.text(600, 600, 'You win!', style);
-            newGameButton=game.add.button(450, 200, 'button', newGame);
-            gameoverMsg.fixToCamera = true;
-            newGameButton.fixToCamera = true;
-          } else {
-            gameOverMsg = me.game.add.text(600, 600, 'You lost =(', style);
-            newGameButton=game.add.button(450, 200, 'button', newGame);
-            gameoverMsg.fixToCamera = true;
-            newGameButton.fixToCamera = true;
-          }
-
-        } else if (p1Alive.alive && !p2Alive.alive) {
-            if (playerNumber === 1) {
-              gameOverMsg = me.game.add.text(600, 600, 'You win!', style);
-              newGameButton=game.add.button(450, 200, 'button', newGame);
-              gameoverMsg.fixToCamera = true;
-              newGameButton.fixToCamera = true;
-            } else {
-              gameOverMsg = me.game.add.text(600, 600, 'You lost =(', style);
-              newGameButton=game.add.button(450, 200, 'button', newGame);
-              gameoverMsg.fixToCamera = true;
-              newGameButton.fixToCamera = true;
-            }
-        } else if (p1Alive.alive && p2Alive.alive) {
-            gameOverMsg = me.game.add.text(600, 600, 'Everybody Win!', style);
-            newGameButton=game.add.button(450, 200, 'button', newGame);
-            gameoverMsg.fixToCamera = true;
-            newGameButton.fixToCamera = true;
-
-        }
-
+        console.log('over');
+        this.game.stage.start('GameOver');
       }
+      //   if (!p1Alive.alive && !p2Alive.alive) {
+      //     gameOverMsg = me.game.add.text(600, 600, 'Everybody Lose!', style);
+      //     newGameButton=game.add.button(450, 200, 'button', newGame);
+      //     gameoverMsg.fixToCamera = true;
+      //     newGameButton.fixToCamera = true;
+      //   } else if (!p1Alive.alive && p2Alive.alive) {
+      //     if (playerNumber === 2) {
+      //       gameOverMsg = me.game.add.text(600, 600, 'You win!', style);
+      //       newGameButton=game.add.button(450, 200, 'button', newGame);
+      //       gameoverMsg.fixToCamera = true;
+      //       newGameButton.fixToCamera = true;
+      //     } else {
+      //       gameOverMsg = me.game.add.text(600, 600, 'You lost =(', style);
+      //       newGameButton=game.add.button(450, 200, 'button', newGame);
+      //       gameoverMsg.fixToCamera = true;
+      //       newGameButton.fixToCamera = true;
+      //     }
+      //
+      //   } else if (p1Alive.alive && !p2Alive.alive) {
+      //       if (playerNumber === 1) {
+      //         gameOverMsg = me.game.add.text(600, 600, 'You win!', style);
+      //         newGameButton=game.add.button(450, 200, 'button', newGame);
+      //         gameoverMsg.fixToCamera = true;
+      //         newGameButton.fixToCamera = true;
+      //       } else {
+      //         gameOverMsg = me.game.add.text(600, 600, 'You lost =(', style);
+      //         newGameButton=game.add.button(450, 200, 'button', newGame);
+      //         gameOverMsg.fixToCamera = true;
+      //         newGameButton.fixToCamera = true;
+      //       }
+      //   } else if (p1Alive.alive && p2Alive.alive) {
+      //       gameOverMsg = me.game.add.text(600, 600, 'Everybody Win!', style);
+      //       newGameButton=game.add.button(450, 200, 'button', newGame);
+      //       gameoverMsg.fixToCamera = true;
+      //       newGameButton.fixToCamera = true;
+      //
+      //   }
+      //
+      // }
 
       if (p1Alive.ready) {
         if (p1Alive.alive) {
