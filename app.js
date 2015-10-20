7b03994291e57a1d509006f85038c0366f97249f
Jared Cross <cross.jared@gmail.com>
2015-10-20 07:50:45 -0600

    test

--- app.js
+++ app.js
@@ -237,10 +237,7 @@ io.on('connection', function (socket) {
   //log to db
   socket.on('playedGame', function (data) {
     var newCount = data.gamesPlayed + 1;
-    users.findOne({'email' : data.email}, function (userDoc) {
-      users.update({gamesPlayed : newCount });
-    });
-  });
+    users.update({_id : data._id}, {$set : {gamesPlayed : newCount}});
 
 });
 
