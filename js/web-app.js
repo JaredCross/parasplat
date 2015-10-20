d1b4dbe9f1994b0fe3718d654d7a822b23af9feb
Jared Cross <cross.jared@gmail.com>
2015-10-19 18:51:01 -0600

    test

--- js/web-app.js
+++ js/web-app.js
@@ -1,13 +1,100 @@
-var app = angular.module('parasplat', ['ngRoute', 'ngAnimate', 'parasplat.services']);
-
 var game = false;
 
-//controller section
+var app = angular.module('parasplat', ['ngRoute', 'ngAnimate']);
 
-app.controller('UsersController', function ($scope, Parasplat) {
+app.factory('loginService', function ($http, $location) {
+  return {
+    isLogged: function () {
+      var $check = $http.post('/checkstatus');
+      return $check;
+    }
+  };
+});
+
+app.run(function ($rootScope, $location, loginService) {
+  $rootScope.user = 'me';
+
+  $rootScope.$on('$routeChangeStart', function (e, current) {
+
+        var connected = loginService.isLogged();
+        connected.then(function (data) {
+          $rootScope.user = data.data;
+        });
+  });
+});
+
+app.factory('Auth', function(){
+var user;
+
+return{
+
+   isLoggedIn : function(data){
+       if (data.data.email) {
+         user = true;
+         return user;
+       } else {
+         user = false;
+         return user;
+       }
+   }
+ };
+});
+
+app.factory('Parasplat', function () {
+
+  return {
+    startGame : function () {
+      //Create a new game that fills the screen
+      game = new Phaser.Game(1200, 600, Phaser.AUTO, 'pleasework');
+
+      //Add all states
+      game.state.add("Boot", Boot);
+      game.state.add("Preload", Preload);
+      game.state.add("GameTitle", GameTitle);
+      game.state.add("Main", Main);
+      game.state.add("GameOver", GameOver);
+
+      //Start the first state
+      game.state.start("Boot");
+
+      socket.emit('joinGameLobby', {user : 'me'});
+
+      var lfg = setInterval(function () {
+        socket.emit('lookingForGame');
+      }, 3000);
+
+
+      socket.on('leaveLFG', function () {
+        clearInterval(lfg);
+      });
+
+    },
+
+
+    destroyGame : function () {
+      game.destroy();
+      socket.emit('leaveGameLobby');
+      socket.emit('leaveGameRoom');
+    }
+  };
+
+});
+
+
+
+//CONTROLLERS
+app.controller('UsersController', function ($scope, $rootScope, Parasplat) {
+  $rootScope.user.email = true;
+  if ($rootScope.user.email) {
+    $scope.loggedIn = true;
+  } else {
+    $scope.loggedIn = false;
+  }
+
+
+
   var socket = io();
   socket.on('clients', function (clients) {
-    console.log(clients);
     $scope.$apply(function () {
       $scope.users = clients.length + ' users online';
     });
@@ -20,6 +107,7 @@ app.controller('UsersController', function ($scope, Parasplat) {
   $scope.endGame = function () {
     Parasplat.destroyGame();
   };
+
 });
 
 app.controller('ApplicationController', function ($scope, $route, $routeParams, $location, $http, Parasplat) {
