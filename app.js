8284f8ed0a607c105c949d329f8b3626bf2d3ca9
Jared Cross <cross.jared@gmail.com>
2015-10-19 21:00:07 -0600

    test

--- app.js
+++ app.js
@@ -82,16 +82,23 @@ app.get('/auth/google',
 
   app.post('/checkstatus', function (req, res) {
     if (req.user) {
-          // users.insert({
-          //   displayName : req.user.data.displayName,
-          //   email : req.user.data.email,
-          //   gamesPlayed : 0,
-          //   gamesWon: 0
-          // }, function (err, userInfo) {
-          //   res.send(userInfo);
-          // });
-          res.send(req.user);
+      var email = req.user.data.email;
+      var displayName = req.user.data.displayName;
+      users.findOne({email : email}, function (err, userInfo) {
+        if (userInfo) {
+          res.send(userInfo);
+        } else {
+          users.insert({
+            displayName : displayName,
+            email : email,
+            gamesPlayed : 0,
+            gamesWon: 0
+          }, function (err, userInfo) {
+            res.send(userInfo);
+          });
         }
+      });
+    }
   });
 
 
