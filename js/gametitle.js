383e6f0277063f0e68c421b2187cf73186b40c0e
Jared Cross <cross.jared@gmail.com>
2015-10-21 21:40:04 -0600

    text corrections. added game instructions

--- js/gametitle.js
+++ js/gametitle.js
@@ -6,16 +6,20 @@ GameTitle.prototype = {
 
     create: function(){
       var start = this;
-      var style = { font: "32px Arial", fill: "#ffffff", wordWrap: true, wordWrapWidth: 500, align: "center" };
-      gameTitle = game.add.text(500, 200, 'Parasplat!', style);
-      gameReady = game.add.text(400, 300, 'Game is Ready! Press Start', style);
+      var style = { font: "32px Arial", fill: "#ffffff", wordWrap: false, wordWrapWidth: 500, align: "center" };
+      gameTitle = game.add.text(500, 100, 'Parasplat!', style);
+      var instructions = game.add.text(490, 200, 'Instructions:', style);
+      var instructions1 = game.add.text(280, 250, "Press the SPACE BAR to open your parachute", style);
+      var instructions2 = game.add.text(230, 300, "BUT don't open it before your opponent does, or you lose", style);
+      gameReady = game.add.text(400, 425, 'Game is Ready! Press Start', style);
 
-      var button = game.add.button(530, 400, 'button', actionOnClick);
+      var button = game.add.button(530, 450, 'button', actionOnClick);
 
       function actionOnClick() {
         socket.emit('pressedStart');
         button.destroy();
-        gameReady.text = 'Waiting for other player';
+        gameReady.x=350;
+        gameReady.text = 'Waiting for other player to press start';
       }
 
       socket.on('receivedReady', function (data) {
